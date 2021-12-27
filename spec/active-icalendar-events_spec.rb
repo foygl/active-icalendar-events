9fe0b3749b06c0dabee9041c90a548147c2b225b
William Starling <william@bugsnag.com>
2021-12-27 16:12:24 +0000

    Add more tests (#3)
    * Add test with deleted events
    
    * Add test with moved events
    
    Co-authored-by: William Starling <william@haystar.uk>

--- spec/active-icalendar-events_spec.rb
+++ spec/active-icalendar-events_spec.rb
@@ -112,4 +112,318 @@ describe ActiveIcalendarEvents do
       }
     }
   end
+
+  it "check weekly, uk timezone over daylight savings, 25 instances, with deletions" do
+    ical_file_path = './spec/ical_files/google_calendar_uk_weekly_1000_to_1300_mwfs_25_instances_with_deletions.ics'
+
+    # Check every active day
+    expected_active_days = [
+      '2022-03-18',
+      '2022-03-20',
+      '2022-03-21',
+      '2022-03-27',
+      '2022-03-28',
+      '2022-03-30',
+      '2022-04-01',
+      '2022-04-03',
+      '2022-04-04',
+      '2022-04-06',
+      '2022-04-08',
+      '2022-04-10',
+      '2022-04-11',
+      '2022-04-13',
+      '2022-04-15',
+      '2022-04-18',
+      '2022-04-20',
+      '2022-04-22',
+      '2022-04-24',
+      '2022-04-25',
+    ]
+
+    expected_deleted_days = [
+      '2022-03-16',
+      '2022-03-23',
+      '2022-03-25',
+      '2022-04-17',
+      '2022-04-27',
+    ]
+
+    # Check a representative selection of inactive days
+    expected_inactive_days = [
+      '2022-03-13',
+      '2022-03-14',
+      '2022-03-15',
+      '2022-03-17',
+      '2022-03-19',
+      '2022-03-22',
+      '2022-03-26',
+      '2022-03-29',
+      '2022-04-26',
+      '2022-04-28',
+      '2022-04-29',
+      '2022-04-30',
+      '2022-05-01',
+      '2022-05-02',
+    ]
+
+    expected_active_times = [
+      '10:00',
+      '11:00',
+      '12:00',
+      '12:59',
+    ]
+
+    expected_inactive_times = [
+      '09:00',
+      '09:59',
+      '13:00',
+      '14:00',
+    ]
+    
+    expected_active_days.each { |day|
+      expected_active_times.each { |time|
+        run_active_events_test(
+          ical_file_path,
+          "#{day} #{time}",
+          'Europe/London',
+          ['Weekly Test']
+        )
+      }
+
+      expected_inactive_times.each { |time|
+        run_active_events_test(
+          ical_file_path,
+          "#{day} #{time}",
+          'Europe/London',
+          []
+        )
+      }
+    }
+
+    (expected_deleted_days + expected_inactive_days).each { |day|
+      (expected_active_times + expected_inactive_times).each { |time|
+        run_active_events_test(
+          ical_file_path,
+          "#{day} #{time}",
+          'Europe/London',
+          []
+        )
+      }
+    }
+  end
+
+  it "check weekly, uk timezone over daylight savings, 25 instances, with deletions and moves" do
+    ical_file_path = './spec/ical_files/google_calendar_uk_weekly_1000_to_1300_mwfs_25_instances_with_deletions_and_moves.ics'
+
+    # Check every active day
+    expected_active_days = [
+      '2022-03-18',
+      '2022-03-21',
+      '2022-03-30',
+      '2022-04-01',
+      '2022-04-03',
+      '2022-04-04',
+      '2022-04-11',
+      '2022-04-13',
+      '2022-04-15',
+      '2022-04-18',
+      '2022-04-20',
+      '2022-04-22',
+      '2022-04-24',
+      '2022-04-25',
+    ]
+
+    expected_deleted_days = [
+      '2022-03-16',
+      '2022-03-23',
+      '2022-03-25',
+      '2022-04-17',
+      '2022-04-27',
+    ]
+
+    expected_moved_days = {
+      '2022-03-20' => {
+        :active_times => [
+          '2022-03-20 16:00',
+          '2022-03-20 17:00',
+          '2022-03-20 18:00',
+          '2022-03-20 18:59',
+        ],
+        :inactive_times => [
+          '2022-03-20 15:59',
+          '2022-03-20 19:00',
+        ]
+      }, # Moved to 16:00
+      '2022-03-27' => {
+        :active_times => [
+          '2022-03-26 16:00',
+          '2022-03-26 17:00',
+          '2022-03-26 18:00',
+          '2022-03-26 18:59',
+        ],
+        :inactive_times => [
+          '2022-03-26 15:59',
+          '2022-03-26 19:00',
+        ]
+      }, # Moved to 16:00 the day before
+      '2022-03-28' => {
+        :active_times => [
+          '2022-03-27 16:00',
+          '2022-03-27 17:00',
+          '2022-03-27 18:00',
+          '2022-03-27 18:59',
+        ],
+        :inactive_times => [
+          '2022-03-27 15:59',
+          '2022-03-27 19:00',
+        ]
+      }, # Moved to 16:00 the day before
+      '2022-04-06' => {
+        :active_times => [
+          '2022-03-26 23:00',
+          '2022-03-27 00:00',
+          '2022-03-27 00:59', # We lost an hour
+        ],
+        :inactive_times => [
+          '2022-03-26 22:59',
+          '2022-03-27 01:00', # We lost an hour
+          '2022-04-06 09:00',
+          '2022-04-06 10:00',
+          '2022-04-06 11:00',
+          '2022-04-06 12:00',
+          '2022-04-06 13:00',
+          '2022-04-06 14:00',
+          '2022-04-06 15:00',
+          '2022-04-06 16:00',
+          '2022-04-06 17:00',
+        ]
+      }, # Moved to 23:00 on the 26th (daylight savings)
+      '2022-04-08' => {
+        :active_times => [
+          '2022-04-09 16:00',
+          '2022-04-09 17:00',
+          '2022-04-09 18:00',
+          '2022-04-09 18:59',
+        ],
+        :inactive_times => [
+          '2022-04-09 15:59',
+          '2022-04-09 19:00',
+          '2022-04-08 09:00',
+          '2022-04-08 10:00',
+          '2022-04-08 11:00',
+          '2022-04-08 12:00',
+          '2022-04-08 13:00',
+          '2022-04-08 14:00',
+          '2022-04-08 15:00',
+          '2022-04-08 16:00',
+          '2022-04-08 17:00',
+        ]
+      }, # Moved to 16:00 the day after
+      '2022-04-10' => {
+        :active_times => [
+          '2022-04-10 23:00',
+          '2022-04-11 00:00',
+          '2022-04-11 01:00',
+          '2022-04-11 02:00',
+          '2022-04-11 02:59',
+        ],
+        :inactive_times => [
+          '2022-04-10 22:59',
+          '2022-04-11 03:00',
+          '2022-04-10 09:00',
+          '2022-04-10 10:00',
+          '2022-04-10 11:00',
+          '2022-04-10 12:00',
+          '2022-04-10 13:00',
+          '2022-04-10 14:00',
+          '2022-04-10 15:00',
+          '2022-04-10 16:00',
+          '2022-04-10 17:00',
+        ]
+      } # Moved to 23:00, increased duration to 4 hours
+    }
+
+    # Check a representative selection of inactive days
+    expected_inactive_days = [
+      '2022-03-13',
+      '2022-03-14',
+      '2022-03-15',
+      '2022-03-17',
+      '2022-03-19',
+      '2022-03-22',
+      '2022-03-26',
+      '2022-03-29',
+      '2022-04-26',
+      '2022-04-28',
+      '2022-04-29',
+      '2022-04-30',
+      '2022-05-01',
+      '2022-05-02',
+    ]
+
+    expected_active_times = [
+      '10:00',
+      '11:00',
+      '12:00',
+      '12:59',
+    ]
+
+    expected_inactive_times = [
+      '09:00',
+      '09:59',
+      '13:00',
+      '14:00',
+    ]
+
+    expected_moved_days.each { |_, times_to_check|
+      times_to_check[:active_times].each { |time|
+        run_active_events_test(
+          ical_file_path,
+          time,
+          'Europe/London',
+          ['Weekly Test']
+        )
+      }
+
+      times_to_check[:inactive_times].each { |time|
+        run_active_events_test(
+          ical_file_path,
+          time,
+          'Europe/London',
+          []
+        )
+      }
+    }
+    
+    expected_active_days.each { |day|
+      expected_active_times.each { |time|
+        run_active_events_test(
+          ical_file_path,
+          "#{day} #{time}",
+          'Europe/London',
+          ['Weekly Test']
+        )
+      }
+
+      expected_inactive_times.each { |time|
+        run_active_events_test(
+          ical_file_path,
+          "#{day} #{time}",
+          'Europe/London',
+          []
+        )
+      }
+    }
+
+    (expected_deleted_days + expected_inactive_days).each { |day|
+      (expected_active_times + expected_inactive_times).each { |time|
+        run_active_events_test(
+          ical_file_path,
+          "#{day} #{time}",
+          'Europe/London',
+          []
+        )
+      }
+    }
+  end
 end
